61a8ffd25afac2f55b74f3dfa9ff12e4faa4f3b6
Joaquin Medina <zanlorenzocacho10@gmail.com>
2024-11-09 21:17:32 -0300

    se termino validaciones para crear usuario admin, cliente y editar cliente

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -10,19 +10,33 @@ import {
 import { Router } from "express";
 import authRole from "../middleware/authRole.js";
 import authTokenJwt from "../middleware/authTokenJwt.js";
+import validateUser from "../helpers/validations/user.validations.js";
 
 const router = Router();
 
 //////////////////POST///////////////////////////////////////////////////////
 //autentica el token de seguridad y luego verifica si el rol es 'admin'
-router.post("/register", authTokenJwt, authRole(["admin"]), signUpUser);
+//valida los datos ingresados
+router.post(
+    "/register",
+    authTokenJwt,
+    authRole(["admin"]),
+    validateUser,
+    signUpUser
+);
 router.post("/login", login);
-router.post("/create-admin", signUpUser);
+router.post("/create-admin", validateUser, signUpUser);
 //////////////////GET///////////////////////////////////////////////////////
 router.get("/", getUsers);
 router.get("/:id", getUserById);
 //////////////////PUT///////////////////////////////////////////////////////
-router.put("/update", authTokenJwt, authRole(["admin"]), updateUserInfo);
+router.put(
+    "/update",
+    authTokenJwt,
+    authRole(["admin"]),
+    validateUser,
+    updateUserInfo
+);
 router.put(
     "/update/status",
     authTokenJwt,
