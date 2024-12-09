da5777d4b08d621d2b15f295be39dfb3779ae8b3
Joaquin Medina <zanlorenzocacho10@gmail.com>
2024-12-09 18:41:04 -0300

    cambios en connection a db

--- src/config/db.js
+++ src/config/db.js
@@ -6,8 +6,8 @@ const { POSTGRES_URI } = process.env;
 const sequelize = new Sequelize(`${POSTGRES_URI}`, {
     dialect: "postgres",
     dialectModule: pg,
-    native: false,
-    logging: false,
+    /* native: false,
+    logging: false, */
 });
 
 export default sequelize;
